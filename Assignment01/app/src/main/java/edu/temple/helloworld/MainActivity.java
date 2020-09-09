5c9a80d6e937639035a64a405a34c0501329c42f
hanzy <hans78905@gmail.com>
2020-09-09 17:10:13 -0400

    Assignment01 second commit : a button added

--- Assignment01/app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ Assignment01/app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,32 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnClick);
+        txt = findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("You have clicked the button");
+            }
+        });
     }
+
+
 }
