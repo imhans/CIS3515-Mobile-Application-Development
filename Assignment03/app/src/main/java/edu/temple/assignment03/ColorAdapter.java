731a39769fbe3d795156da9728f914de6a21c566
hanzy <hans78905@gmail.com>
2020-09-24 17:24:59 -0400

    Created ColorAdapter.java file

---
+++ Assignment03/app/src/main/java/edu/temple/assignment03/ColorAdapter.java
@@ -0,0 +1,45 @@
+package edu.temple.assignment03;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ColorAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<String> colors;
+
+    public ColorAdapter(Context context, ArrayList<String> colors) {
+        this.context = context;
+        this.colors = colors;
+    }
+    @Override
+    public int getCount() {
+        return colors.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return colors.get(position);
+    }
+
+    @Override //not using
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //Create a View
+        TextView textView = new TextView(context);
+        //Set things for the View
+        textView.setPadding(8, 8, 8, 8);
+        textView.setText(getItem(position).toString());
+
+        //Return the View
+        return textView;
+    }
+}
