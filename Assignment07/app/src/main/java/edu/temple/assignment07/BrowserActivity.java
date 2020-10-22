250c73a6b5b350795b5d778f6e32fbf16516518f
hanzy <hans78905@gmail.com>
2020-10-21 20:11:36 -0400

    Updated two Fragments

--- Assignment07/app/src/main/java/edu/temple/assignment07/BrowserActivity.java
+++ Assignment07/app/src/main/java/edu/temple/assignment07/BrowserActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class BrowserActivity extends AppCompatActivity {
 
+    PageControlFragment pcf;
+    PageViewerFragment pvf;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pcf = new PageControlFragment();
+        pvf = new PageViewerFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.page_control, pcf)
+                .add(R.id.page_viewer, pvf)
+                .commit();
     }
 }
