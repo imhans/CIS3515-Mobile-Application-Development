1d76e4cfbd7f4c545140baa5749d0a10e80783d3
hanzy <hans78905@gmail.com>
2020-11-27 04:26:12 -0500

    Created TextView and ImageButton into each row for the ListView with test data

---
+++ Assignment07/app/src/main/java/edu/temple/assignment07/Bookmark.java
@@ -0,0 +1,20 @@
+package edu.temple.assignment07;
+
+public class Bookmark {
+    String url;
+    String title;
+
+    public Bookmark(String url, String title) {
+        this.url = url;
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
--- Assignment07/app/src/main/java/edu/temple/assignment07/BookmarkActivity.java
+++ Assignment07/app/src/main/java/edu/temple/assignment07/BookmarkActivity.java
@@ -5,8 +5,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,57 +17,78 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class BookmarkActivity extends AppCompatActivity {
-
 
     ListView listView;
     bookmarkAdapter mAdapter;
     Button closeButton;
+    ArrayList<Bookmark> bms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmark);
 
-        //listView = findViewById(R.id.lvBookmark);
-        //ArrayAdapter mAdapter = new ArrayAdapter<String>(this.getApplicationContext(), android.R.layout.simple_list_item_2);
-        //mAdapter = new bookmarkAdapter(this.getApplicationContext(), android.R.layout.simple_list_item_2);
-        //listView.setAdapter(mAdapter);
+        listView = findViewById(R.id.lvBookmark);
+
+        bms = new ArrayList<Bookmark>();
+        Bookmark a = new Bookmark("http://google.com", "Google");
+        Bookmark b = new Bookmark("http://google.com", "Google2");
+        bms.add(a); bms.add(b);
+        String c = Integer.toString(bms.size());
+        Log.d("BMs", c);
+
+        mAdapter = new bookmarkAdapter(this, android.R.layout.simple_list_item_2, bms);
+
+        listView.setAdapter(mAdapter);
 
     }
 
 
-    public class bookmarkAdapter extends ArrayAdapter<String> {
+    public class bookmarkAdapter extends ArrayAdapter<Bookmark> {
 
         Context context;
-        ImageButton deleteButton;
+        ArrayList<Bookmark> bookmarks;
 
-        public bookmarkAdapter(@NonNull Context context, int resource) {
-            super(context, resource);
+
+        public bookmarkAdapter(@NonNull Context context, int resource, ArrayList<Bookmark> bookmarks) {
+            super(context, resource, bookmarks);
             this.context = context;
-
+            this.bookmarks = bookmarks;
         }
 
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
+            View view = convertView;
             TextView textView;
+            ImageButton deleteButton;
 
-            if (convertView instanceof TextView) {
-                textView = (TextView) convertView;
-
-            } else {
-                textView = new TextView(context);
-                textView.setPadding(5,8,8,5);
-                textView.setGravity(Gravity.CENTER);
-                textView.setTextSize(14);
-                textView.setText("Blank");
+            if ( view == null ) {
+                view = View.inflate(getContext(), R.layout.bookmark_list_item, null);
             }
-            textView.setText("Testing");
 
-            return textView;
+            textView = view.findViewById(R.id.tvTitle);
+            deleteButton = view.findViewById(R.id.btnDelete);
+
+            textView.setPadding(0,26,0,20);
+            textView.setTextSize(20);
+            textView.setTextColor(Color.BLACK);
+            textView.setText(bookmarks.get(position).getTitle());
+
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "it works", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            return view;
         }
     }
 }
